7e033df9f65ab211e60d1997ad3f7ac49f5ee774
LeLoi-Hunter <loildce182275@fpt.edu.vn>
2026-06-13 13:31:12 +0700

    push code Train-02-013

---
+++ Train-02-013/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    // setting slide
+    currentSlide = "013";
+    localStorage.setItem("current-slide", currentSlide);
+    disableSwipe = false;
+  
+    // flow-slide
+    var el = document.getElementById("container");
+    swipedetect(el, function (swipedir) {
+      if (swipedir == "left") {
+        window.goToSlide("014");
+      }
+      if (swipedir == "right") {    
+        window.goToSlide("012");
+      }
+    });
+  
+    // animation
+    const animations = {
+        100: [".content-title"],
+        400: [".content-desc"],
+        900: [".btn-next"],
+    };
+
+    for (const [offset, elem] of Object.entries(animations)) {
+        setTimeout(() => elem.forEach((e) => $(e).addClass("show")), offset);
+    }
+});
+  
